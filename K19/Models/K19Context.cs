48fed7cfeba9ac32b105f8c467caa8e5c2e91c9c
akemioshiro@hotmail.com <akemioshiro@hotmail.com>
2017-04-11 23:04:53 -0300

    K19

---
+++ K19/Models/K19Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace K19.Models
+{
+    public class K19Context:DbContext
+    {
+        public DbSet<Editora> Editoras { get; set; }
+
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
---
+++ K19/Models/Livro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K19.Models
+{
+    public class Livro
+    {
+        public int LivroID { get; set; }
+        public string Titulo { get; set; }
+        public double Preco { get; set; }
+        public int EditoraID { get; set; }
+        public virtual  Editora Editora { get; set; }
+    }
+}
